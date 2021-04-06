b73b2a812f4a09a3df331c5780aff51a30cd73d3
Junsu-Choi <lezam682@gmail.com>
2021-04-07 00:59:23 +0900

    검색기능 추가

--- app/src/main/java/com/jschoi/clone/weatherapp/View/Activity/MainActivity.kt
+++ app/src/main/java/com/jschoi/clone/weatherapp/View/Activity/MainActivity.kt
@@ -2,7 +2,11 @@ package com.jschoi.clone.weatherapp.View.Activity
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jschoi.clone.weatherapp.Network.RetrofitClient
 import com.jschoi.clone.weatherapp.Network.RetrofitService
@@ -13,39 +17,77 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private val TAG = javaClass.simpleName
 
     private lateinit var retrofit: Retrofit
     private lateinit var supplementService: RetrofitService
 
-    private lateinit var tvTemperature: TextView
+    private val tvTemperature: TextView by lazy {
+        findViewById(R.id.tv_temperature)
+    }
+    private val edCity: EditText by lazy {
+        findViewById(R.id.ed_city)
+    }
+    private val btnSearch: ImageButton by lazy {
+        findViewById(R.id.btn_search)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tvTemperature = findViewById(R.id.tv_temperature);
-
-        initRetrofit()
-    }
-
-
-    private fun initRetrofit() {
         retrofit = RetrofitClient.getInstance()
         supplementService = retrofit.create(RetrofitService::class.java)
 
-        supplementService.reqeustSample("seoul", getString(R.string.weather_app_key))
+        btnSearch.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        Log.d(TAG, "OnClick...")
+        when (v!!.id) {
+            btnSearch.id -> if (edCity.toString().isNullOrEmpty()) {
+                return
+            } else {
+                initRetrofit(edCity.text.toString())
+            }
+        }
+    }
+
+    private fun initRetrofit(searchData: String) {
+        supplementService.reqeustSample(searchData, getString(R.string.weather_app_key))
             .enqueue(object : Callback<ResSample> {
                 override fun onResponse(call: Call<ResSample>, response: Response<ResSample>) {
-                    val res = response.body()!!
-                    // 온도
-                    tvTemperature.text = String.format("%d°C", (res.main.temp - 273.15).toInt())
+                    val res = response.body()
+                    if (res != null) {
+                        // 온도
+                        tvTemperature.text = String.format("%d°C", (res.main.temp - 273.15).toInt())
+                    } else {
+                        showToast(false, null)
+                    }
                 }
 
                 override fun onFailure(call: Call<ResSample>, t: Throwable) {
                     Log.d(TAG, ">>>>>>>>>>>>>>>>>>>>> ${t}")
                 }
             })
+    }
+
+
+    /**
+     * Show Toast Message
+     */
+    private fun showToast(isLongDuration: Boolean, message: String?) {
+        var duration = Toast.LENGTH_SHORT
+
+        if (message.isNullOrEmpty()) {
+            message.apply {
+                getString(R.string.error_network)
+            }
+        }
+        if (isLongDuration) {
+            duration = Toast.LENGTH_LONG
+        }
+        Toast.makeText(this@MainActivity, message, duration).show()
     }
 }
